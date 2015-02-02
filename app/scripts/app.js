6fc625721b205602e15b68aa80caf8ce167d6feb
Ilia <ilia@cellrox.com>
2015-02-02 16:56:31 +0200

    use yeoman angular-app to scaffold the project

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name personaMarketApp
+ * @description
+ * # personaMarketApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('personaMarketApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personaMarketApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the personaMarketApp
+ */
+angular.module('personaMarketApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
